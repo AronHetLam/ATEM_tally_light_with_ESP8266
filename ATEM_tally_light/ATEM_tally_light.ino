059fab50553edd868360a74f6ea21b978fbb646b
Aron het Lam <aronhetlam@gmail.com>
2020-09-01 01:29:47 +0200

    Added header

--- ATEM_tally_light/ATEM_tally_light.ino
+++ ATEM_tally_light/ATEM_tally_light.ino
@@ -1,3 +1,23 @@
+/*
+Copyright (C) 2020 Aron N. Het Lam, aronhetlam@gmail.com
+
+This program makes an ESP8266 into a wireless tally light system for ATEM switchers,
+by using Kasper Skårhøj's (<https://skaarhoj.com>) ATEM clinet libraries for Arduino.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 //Include libraries:
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>

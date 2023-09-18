9df5638459b07fdfc2ca3a54bfc09212240e4b08
AronHetLam <aronhetlam@gmail.com>
2023-09-18 22:54:59 +0200

    Add ATEM_tally_light.ino for Arduino IDE compatibility

---
+++ ATEM_tally_light/ATEM_tally_light.ino
@@ -0,0 +1,29 @@
+/*
+    Copyright (C) 2023 Aron N. Het Lam, aronhetlam@gmail.com
+
+    This program makes an ESP8266 into a wireless tally light system for ATEM switchers,
+    by using Kasper Skårhøj's (<https://skaarhoj.com>) ATEM client libraries for Arduino.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "ATEM_tally_light.hpp"
+
+/*
+    Where did all the code go?!
+
+    It's all moved to "ATEM_tally_light.cpp" for better compatibility with PlatformIO.
+    
+    This .ino file is kept for compatibility with the Arduino IDE
+*/

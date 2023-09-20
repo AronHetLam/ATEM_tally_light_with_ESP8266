6e1988d6b5a7febb5190d81a0d0b7271f3aeb594
AronHetLam <aronhetlam@gmail.com>
2023-09-20 14:23:24 +0200

    ...

--- pio_scripts/publish_binaries.py
+++ pio_scripts/publish_binaries.py
@@ -4,14 +4,14 @@ import json
 Import('env')
 
 PIOENV = env.get("PIOENV")
-VERSION = env.get("VERSION", "")
+VERSION = os.getenv("VERSION", "")
 BOARD_CONFIG = env.BoardConfig()
 APP_BIN = "{}{}{}.bin".format(
     env.get("BUILD_DIR"), os.path.sep, env.get("PROGNAME"))
 PUBLISH_DIR_BASE = "publish{}".format(os.path.sep)
 PUBLISH_DIR_FULL = "{}{}{}".format(PUBLISH_DIR_BASE, PIOENV, os.path.sep)
-PUBLISH_BIN = "{}ATEM_tally_light_{}_{}.bin".format(PUBLISH_DIR_FULL, VERSION, PIOENV)
-
+PUBLISH_BIN = "{}ATEM_tally_light_{}_{}.bin".format(
+    PUBLISH_DIR_FULL, VERSION, PIOENV)
 
 def _get_cpp_define_value(env, define: str):
     define_list = [(item[-1] if item[0] == define else "")
@@ -21,14 +21,16 @@ def _get_cpp_define_value(env, define: str):
 
     return None
 
+
 def _create_publish_dir():
     if not os.path.isdir(PUBLISH_DIR_BASE):
         os.mkdir(PUBLISH_DIR_BASE)
-    
+
     if not os.path.isdir(PUBLISH_DIR_FULL):
         os.mkdir(PUBLISH_DIR_FULL)
 
-def _esp_webtools_manifest(env, chip_family, flash_images):
+
+def _esp_webtools_manifest(env, chip_family, flash_parts):
     is_test_server = _get_cpp_define_value(
         env, "TALLY_TEST_SERVER") is not None
     manifest = {
@@ -39,41 +41,44 @@ def _esp_webtools_manifest(env, chip_family, flash_images):
             "chipFamily": chip_family,
             "parts": [
                 {
-                    "path": "{}{}".format(PIOENV, os.path.basename(image[1])),
+                    "path": "{}{}".format(PUBLISH_DIR_FULL, os.path.basename(image[1])),
                     "offset": int(image[0], 16)
-                } for image in flash_images]
+                } for image in flash_parts]
         }]
     }
 
     with open("{}manifest.json".format(PUBLISH_DIR_FULL), "w") as outfile:
         json.dump(manifest, outfile)
 
+
 def bin_rename_copy(source, target, env):
     _create_publish_dir()
 
     chip_family = _get_cpp_define_value(env, "CHIP_FAMILY")
-    flash_images = env.get("FLASH_EXTRA_IMAGES", []) + \
-        [(env.get("ESP32_APP_OFFSET", "0x00"), str(target[0]))]
+    flash_images = env.get("FLASH_EXTRA_IMAGES", [])
 
-    if (chip_family.startswith("ESP32")):
-        # Run esptool to merge images into a single binary
-        env.Execute(" ".join(
-            [
-                "esptool",
-                "--chip", BOARD_CONFIG.get("build.mcu", "esp32"),
-                "merge_bin",
-                "-o", PUBLISH_BIN,
-                *env.Flatten(flash_images)
-            ]
-        ))
+    # if (chip_family.startswith("ESP32")):
+    #     # Run esptool to merge images into a single binary
+    #     env.Execute(" ".join(
+    #         [
+    #             "esptool",
+    #             "--chip", BOARD_CONFIG.get("build.mcu", "esp32"),
+    #             "merge_bin",
+    #             "-o", PUBLISH_BIN,
+    #             *env.Flatten(flash_images)
+    #         ]
+    #     ))
 
-    else:  # esp8266
-        shutil.copy(str(target[0]), PUBLISH_BIN)
+    # else:  # esp8266
+    for image in flash_images:
+        shutil.copy(image[1], PUBLISH_DIR_FULL)
+    shutil.copy(str(target[0]), PUBLISH_BIN)
 
-    _esp_webtools_manifest(env, chip_family, flash_images)
+    _esp_webtools_manifest(env, chip_family, flash_images +
+                           [(env.get("ESP32_APP_OFFSET", "0x00"), PUBLISH_BIN)])
 
 
-if (_get_cpp_define_value(env, "PUBLISH") is not None):
+if (os.getenv("PUBLISH") is not None):
     env.AddPostAction("$BUILD_DIR/${PROGNAME}.bin", [bin_rename_copy])
 
     env.Replace(

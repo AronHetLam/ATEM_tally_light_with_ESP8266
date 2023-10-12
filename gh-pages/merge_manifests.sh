9b773568953082d8b40147356d005017d09efd8a
AronHetLam <aronhetlam@gmail.com>
2023-10-12 16:03:20 +0200

    Fix missing envs

--- gh-pages/merge_manifests.sh
+++
@@ -1,7 +0,0 @@
-#! bin/bash
-
-echo "$(echo $NAMES | jq -r '.[]')" | while read -r name; do
-    sanitized_name=$(echo \"$name\" | jq -r 'sub("(\\s)"; "_"; "g")')
-    echo "Processing $name"
-    jq -s --arg name "$name" '[.[] | select(.name == $name)] | (.[0] | with_entries(select(.key != "builds"))) + {builds: map(.builds) | add}' ./_site/publish/*/manifest.json > ./_site/publish/${sanitized_name}_manifest.json
-done
